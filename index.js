e57daa5089a30232dc8472e878efdd47425b77c8
ricardokorzekwa <korzekwa.ricardo@gmail.com>
2022-04-13 16:26:05 -0300

    Substitui o espaço vazio do array por uma let aux

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 function positions(firstPlace, secondPlace, lastPlace){
     let atleta = [];
-    atleta[0] = " ";
-    atleta[1] = firstPlace;
-    atleta[2] = secondPlace;
-    atleta[3] = lastPlace;
+    atleta[0] = firstPlace;
+    atleta[1] = secondPlace;
+    atleta[2] = lastPlace;
+    let aux = 0
     
-    if(atleta[2] == `Daniel`){
+    if(atleta[1] == `Daniel`){
+        aux       = atleta[0]
         atleta[0] = atleta[1]
-        atleta[1] = atleta[2]
-        atleta[2] = atleta[0]
-        return console.log(`1ª - Colocado ${atleta[1]} -- 2ª - Colocado ${atleta[2]} -- 3ª - Colocado ${atleta[3]}`)
+        atleta[1] = aux
+        
+        return console.log(`1ª - Colocado ${atleta[0]} -- 2ª - Colocado ${atleta[1]} -- 3ª - Colocado ${atleta[2]}`)
 
-    }else if(atleta[3] == `Daniel`) {
-        atleta[0] = atleta[2]
-        atleta[2] = atleta[3]
-        atleta[3] = atleta[0]
-        return console.log(`1ª - Colocado ${atleta[1]} -- 2ª - Colocado ${atleta[2]} -- 3ª - Colocado ${atleta[3]}`)
+    }else if(atleta[2] == `Daniel`) {
+        aux       = atleta[1]
+        atleta[1] = atleta[2]
+        atleta[2] = aux
+        return console.log(`1ª - Colocado ${atleta[0]} -- 2ª - Colocado ${atleta[1]} -- 3ª - Colocado ${atleta[2]}`)
     }else{
-        return console.log(`1ª - Colocado ${atleta[1]} -- 2ª - Colocado ${atleta[2]} -- 3ª - Colocado ${atleta[3]}`)
+        return console.log(`1ª - Colocado ${atleta[0]} -- 2ª - Colocado ${atleta[1]} -- 3ª - Colocado ${atleta[2]}`)
 
     }
     
 }
-positions(`Pedro`,`Daniel` , `Carlos`);
+positions(`Daniel`, `Pedro`,`Carlos`);
+positions(`Pedro`,`Daniel`,`Carlos`);
+positions(`Pedro`,`Carlos`,`Daniel` );
